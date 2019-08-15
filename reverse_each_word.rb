788f6db7e07cdff8e7cbda811733a031c722d3e9
Mark Enright <enright.mark@gmail.com>
2019-08-15 01:45:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(word)  #string
   word = word.split(" ")  #array
   
   new_word = []
-  word.each do |rev|
+  word.collect do |rev|
    new_word << rev.reverse
     
   end
@@ -16,3 +16,6 @@ end
 #its in the array, have to figure out how to get back to string
 
 reverse_each_word("Hello there, and how are you?")
+
+
+
