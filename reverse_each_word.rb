0cdd4910be99be40242f108558c53dfca00c43f5
Mark Enright <enright.mark@gmail.com>
2019-08-15 01:45:45 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+def reverse_each_word(word)  #string
+  
+  word = word.split(" ")  #array
+  
+  new_word = []
+  word.collect do |rev|
+   new_word << rev.reverse
+    
+  end
+  #p new_word  #array
+  new_word = new_word.join(" ")
+  new_word #string
+  
+end
+
+#its in the array, have to figure out how to get back to string
+
+reverse_each_word("Hello there, and how are you?")
+
+
+
---
+++ tester.rb
@@ -0,0 +1,10 @@
+str = "Hello there, and how are you?"
+puts str
+str =  str.split()
+new_arr = []
+str.each do |part|
+  new_arr << part.reverse
+end
+p new_arr
+p new_arr.join(" ")
+
